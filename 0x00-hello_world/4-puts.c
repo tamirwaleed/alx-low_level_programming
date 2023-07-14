d0420bf142a723ce124f9c11a57905742ac4441a
Tamir <hero2000.ts@gmail.com>
2023-07-14 05:01:19 -0700

    teemo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * 
+ * Description: output using puts
+ * using character array
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void) /*whatevr*/
 {
 char String_[] = "\"Programming is like building a multilingual puzzle";
