b11e9c202fd695ee595a965f4c85bfa7f5a336be
Tamir <hero2000.ts@gmail.com>
2023-07-14 05:14:12 -0700

    teemo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * whatever
- * Return 0 for success
-*/
+ * main - Entry point
+ * Description: output using printf
+ * using constant string
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");	
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
