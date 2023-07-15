a02f84781cf5a31057cc1fd08a51b37a96e945fa
Tamir <hero2000.ts@gmail.com>
2023-07-14 19:18:32 -0700

    teemo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - Entry point
+* Description: prints last integer
+* Using different headers
+* Return: Always success 0
+*/
+int main(void)
+{
+int c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+putchar('\n');
+}
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0-positive_or_negative.c prints whether the number stored in the variable n is positive or negative
 1-last_digit.c prints the last digit of the number stored in the variable n
+2-print_alphabet.c  prints the alphabet in lowercase, followed by a new line
