a88778d316b86014c99d2d297e0ce28bd0efba3b
Tamir <hero2000.ts@gmail.com>
2023-07-18 17:36:53 -0700

    teemo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * main - Entry point
 * Description: prints putchar
-* Using different headers
+* Using defined headers
 * Return: Always success 0
 */
 int main(void)
 {
 char charstring[50] = "_putchar\n";
-for (int i = 0; i <= 50; i++)
-putchar(charstring[i]);
+int i;
+for (i = 0; i <= 50; i++)
+_putchar(charstring[i]);
 return (0);
 }
