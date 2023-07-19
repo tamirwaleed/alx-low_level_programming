0be3678118bc6cb6b564bc938fd61e6764886a1a
Tamir <hero2000.ts@gmail.com>
2023-07-19 10:56:20 -0700

    teemo

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,24 +5,24 @@
 void times_table(void)
 {
 int a, b;
-for(a = 0; a <= 9; a++)
+for (a = 0; a <= 9; a++)
 {
-for(b = 0; b <= 9; b++)
+for (b = 0; b <= 9; b++)
 {
 int c;
 c = a * b;
-if(c >= 10)
+if (c >= 10)
 {
 _putchar((c / 10) + '0');
 _putchar((c % 10) + '0');
 }
 else
 _putchar(c + '0');
-if(b != 9)
+if (b != 9)
 {
 _putchar(',');
 _putchar(' ');
-if((a * (b+1)) < 10)
+if ((a * (b + 1)) < 10)
 _putchar(' ');
 }
 }
