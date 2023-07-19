91a40540731b59dade38dcd833b01734c3f8f9fb
Tamir <hero2000.ts@gmail.com>
2023-07-18 17:47:05 -0700

    teemo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - Entry point
+* Description: prints alphabet
+* Using defined headers
+* Return: Always success 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z' ; c++)
+_putchar(c);
+_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 0-putchar.c prints _putchar, followed by a new line
+1-alphabet.c prints the alphabet, in lowercase, followed by a new line
