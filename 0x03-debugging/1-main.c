9d965af91c656569c51c5d3348618656d70f7924
Tamir <hero2000.ts@gmail.com>
2023-07-20 07:44:18 -0700

    teemo

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 10;
+
+while (i < 10)
+{
+putchar(i);   /* no increment for i, thus it will always be less than 10 */
+}
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
