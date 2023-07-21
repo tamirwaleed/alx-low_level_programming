24c4007d060d530403a298b2a18d34e5f7c286e0
Tamir <hero2000.ts@gmail.com>
2023-07-21 05:47:32 -0700

    teemo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,18 +11,18 @@ for (i = 1; i <= 10; i++)
 for (j = 0; j <= 14; j++)
 {
 if (j <= 9)
-_putchar(j + '0'); 
+_putchar(j + '0');
 else
 {
 for (x = 0; x <= 1; x++)
 {
 if (x == 0)
 y = j / 10;
-else 
+else
 y = j  % 10;
 _putchar(y + '0');
 }
-}      
+}    
 }
 _putchar('\n');
 }
