7583203943925cceade3cf8716bc9479a126da14
Tamir <hero2000.ts@gmail.com>
2023-07-24 11:06:11 -0700

    teemo

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets to 98
+ * @*n: integer pointer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+return;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98.c resets to 98
