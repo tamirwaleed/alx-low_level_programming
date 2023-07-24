2bb873a239aec3282084fba907312ec9912d4884
Tamir <hero2000.ts@gmail.com>
2023-07-24 14:54:09 -0700

    teemo

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,17 +2,15 @@
 /**
  * *_strcpy - copies
  * @dest: pointer
- * @src: string to be copied 
-* Return: pointer to dest
+ * @src: string to be copied
+ *Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-char d[1000];
 int i, j = 0;
 for (i = 0; src[i] != '\0'; i++)
 j++;
 for (i = 0; i <= j; i++)
-d[i] = src[i];
-*dest = d[0];
-return dest;
+dest[i] = src[i];
+return (dest);
 }
