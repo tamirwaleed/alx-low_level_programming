0eb88fbdf8ea0f804b83128ec936782e0ac043bb
Tamir <hero2000.ts@gmail.com>
2023-07-24 14:10:41 -0700

    teemo

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,9 +6,13 @@
  */
 void puts2(char *str)
 {
-int i;
-for (i = 0 ; str[i] != '\0'; i = i + 2)
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
 _putchar(str[i]);
+i++;
+}
 _putchar('\n');
 return;
 }
