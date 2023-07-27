d26faa8e33fd2118f00f124eaddaaed396f02360
Tamir <hero2000.ts@gmail.com>
2023-07-27 09:14:13 -0700

    teemo

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,19 +4,23 @@
  * @c: string
  *Return: c
  */
-char *cap_string(char *s)
+char *cap_string(char *c)
 {
-int i, j;
+int i, j, x, change1 = 0;
 char sep[] = " \t\n,;.!?\"(){}";
-i = 1;
-if (s[0] >= 'a' && s[0] <= 'z')
-s[0] -= ('a' - 'A');
-while (s[i] != '\0')
+for (i = 0, j = 1; c[i + 1] != '\0'; i++, j++)
 {
-for (j = 0; sep[j] != '\0'; j++)
-if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
-s[i] -= ('a' - 'A');
-i++;
+for (x = 0; sep[x] != '\0'; x++)
+{
+if (c[i] == sep[x])
+change1 = 1;
 }
-return (s);
+for (x = 0; sep[x] != '\0'; x++)
+{
+if (c[j] != sep[x] && change1 == 1 && c[j] <= 122 && c[j] >= 97)
+c[j] = c[j] - 32;
+change1 = 0;
+}
+}
+return (c);
 }
