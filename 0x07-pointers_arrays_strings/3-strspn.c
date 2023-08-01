3b50cc15b6a5f1a48caa77eabe0b447f9c0f76e3
Tamir <hero2000.ts@gmail.com>
2023-08-01 12:16:02 -0700

    teemo

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strspn - locates
+ * @s: to be counted in
+ * @accept: checked 
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i = 0, j, k;
+for (j = 0; accept[j] != '\0'; j++)
+{
+for (k = 0; s[k] != '\0' && s[k] != 32; k++) 
+{
+if (accept[j] == s[k])
+i++;
+}
+}
+return (i);
+}
