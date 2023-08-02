712b129e5d180190db17399e29f13bdfe7662e4e
Tamir <hero2000.ts@gmail.com>
2023-08-02 13:13:27 -0700

    teemo

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - finds factorial
+ * @n: integer
+ * Return: factorial
+ */
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+else
+return (n * factorial(n -1));
+}
