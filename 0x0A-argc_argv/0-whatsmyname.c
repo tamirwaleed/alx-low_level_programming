27b76db7720432d61bfb961265efd067cdbb4e32
Tamir <hero2000.ts@gmail.com>
2023-08-04 14:34:28 -0700

    teemo

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints filename
+ * @argc: number of arguments
+ * @argv: string of arguments
+ * Return: Always success 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
