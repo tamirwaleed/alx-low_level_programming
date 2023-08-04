b0a73007decbe2b36f291b9b37c7dde1800444fc
Tamir <hero2000.ts@gmail.com>
2023-08-04 14:56:06 -0700

    teemo

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/** 
+ * main - prints all arguments
+ * @argc: number
+ * @argv: arguments
+ * Return: ALways
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
