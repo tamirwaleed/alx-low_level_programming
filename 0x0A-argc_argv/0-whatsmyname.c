cca6691fdf9ddf6727631170cc7df74c2687c497
Tamir <hero2000.ts@gmail.com>
2023-08-04 14:45:06 -0700

    teemo

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: string of arguments
  * Return: Always success 0
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
