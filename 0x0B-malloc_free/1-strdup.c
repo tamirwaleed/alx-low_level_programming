76cde08c9a9a31b3a639ddea1013007faf41d5a8
Tamir <hero2000.ts@gmail.com>
2023-08-07 13:24:10 -0700

    teemo

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,14 +8,14 @@
 char *_strdup(char *str)
 {
 char *s;
-int i, size = 0;
-if (*str == '\0')
-return ('\0');
+unsigned int i, size = 0;
+if (str == NULL)
+return (NULL);
 while (str[size] != '\0')
 size++;
-s = malloc(sizeof(char) * (1 + size));
-if (*s == '\0')
-return ('\0');
+s = malloc(sizeof(char) * (size + 1));
+if (s == NULL)
+return (NULL);
 for (i = 0; str[i] != '\0'; i++)
 s[i] = str[i];
 return (s);
