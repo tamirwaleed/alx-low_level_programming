6bf37fc592959dc0ccd793607c7d000050e843f0
Tamir <hero2000.ts@gmail.com>
2023-08-23 07:02:55 -0700

    teemo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+typedef struct dog 
 {
 char *name;
 float age;
@@ -9,7 +9,6 @@ char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 
