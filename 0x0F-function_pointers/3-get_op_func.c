8d31637822fd67bc0453f535bcee63cac70e10cc
Tamir <hero2000.ts@gmail.com>
2023-08-24 12:40:41 -0700

    teemo

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * get_op_func - gets operand
+ *@s: string
+ * Return: pointer to required function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].f != NULL)
+{
+if (*s == *(ops[i].op) && s[1] == '\0')
+return (ops[i].f);
+i++;
+}
+printf("Error\n");
+exit(99);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds
+ *@a: 1st
+ *@b: 2nd
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - subtracts
+ *@a: 1st
+ *@b: 2nd
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - multiplies
+ *@a: 1st
+ *@b: 2nd
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - divides
+ *@a: 1st
+ *@b: 2nd
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - modulus
+ *@a: 1st
+ *@b: 2nd
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
