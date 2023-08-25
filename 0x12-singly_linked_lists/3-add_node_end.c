2c28f05817d1851d86db5664e58ace27db42abd3
Tamir <hero2000.ts@gmail.com>
2023-08-24 18:38:03 -0700

    teemo

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,12 +9,17 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new = malloc(sizeof(list_t)), *last;
-last = *head;
 if (new == NULL)
 return (NULL);
+last = *head;
 new->str = strdup(str);
 new->len = strlen(str);
 new->next = NULL;
+if (*head == NULL)
+{
+*head = new;
+return (*head);
+}
 while (last->next != NULL)
 last = last->next;
 last->next = new;
