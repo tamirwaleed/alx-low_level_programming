d115b85b230ad223e80201372b4b2fbd84f833d7
Tamir <hero2000.ts@gmail.com>
2023-08-25 06:46:05 -0700

    teemo

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,8 +11,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list ap;
 unsigned int i;
 va_start(ap, n);
-if (n == 0)
-return;
 for (i = 0; i < n; i++)
 {
 if (i != n - 1)
@@ -20,8 +18,6 @@ if (i != n - 1)
 printf("%i", va_arg(ap, int));
 if (separator != NULL)
 printf("%s", separator);
-else
-printf("%s", va_arg(ap, char *));
 }
 else
 printf("%i", va_arg(ap, int));
