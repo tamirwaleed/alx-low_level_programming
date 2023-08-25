04511e1a96eec4631efd64b6f1955c0cdb26cb32
Tamir <hero2000.ts@gmail.com>
2023-08-24 18:03:10 -0700

    teemo

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - prints list contents
+ * @h: pointer
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+unsigned int i = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+i++;
+h = h->next;
+}
+return (i);
+}
