bc84a43e9fa66a09cfa5399eb2ad07d2eaef315c
tamirwaleed <hero2000.TS@gmail.com>
2023-11-13 17:24:03 -0800

    teemo

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/tamirwaleed/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libdynamic.so
+wget -P /tmp https://github.com/tamirwaleed/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libdynamic.so
 export LD_PRELOAD=/tmp/libdynamic.so
 
