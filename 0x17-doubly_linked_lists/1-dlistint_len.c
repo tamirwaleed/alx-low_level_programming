a7bf0121b6bcfd47fb1b63675fe7a370f4bbe1cf
tamirwaleed <hero2000.TS@gmail.com>
2023-11-15 09:57:58 -0800

    teemo

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,12 +2,12 @@
 /**
  * dlistint_len - prints number
  * @h: head
- * Return: Number 
+ * Return: Number
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
-	
+
 	while (h)
 	{
 		h = h->next;
