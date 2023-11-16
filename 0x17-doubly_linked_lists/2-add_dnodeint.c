6e2334e74ff070bfc98535c8cc39d5d1eca6a31c
tamirwaleed <hero2000.TS@gmail.com>
2023-11-16 06:57:31 -0800

    teemo

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds node
+ * @head: head
+ * @n: data
+ * Return: address or NULL
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (!new || !head)
+	return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	if (!*head)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
+	return (new);
+}
