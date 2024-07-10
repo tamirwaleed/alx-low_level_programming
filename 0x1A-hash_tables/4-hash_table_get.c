c2310076a15322c65bdb339e702e3950aef188be
Tamir <hero2000.ts@gmail.com>
2024-07-10 11:18:52 -0700

    Another ALX Task

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp;
 
 	if (!ht || !ht->array || !ht->size ||
-	    !key || strlen(key == 0))
+	    !key || strlen(key) == 0)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
