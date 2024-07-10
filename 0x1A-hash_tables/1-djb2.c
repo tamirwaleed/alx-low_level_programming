1a5cf3720c679e97d6d3baa9c3f68b12522d6879
Tamir <hero2000.ts@gmail.com>
2024-07-10 08:01:20 -0700

    Another ALX Task

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function with the djb2 algorithm
+ * @str: string to be hashed
+ * Return: the hashed value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+
+	return (hash);
+}
