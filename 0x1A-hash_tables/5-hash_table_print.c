67c6538d4bec5182ab39f2a5b075cf7102353365
Tamir <hero2000.ts@gmail.com>
2024-07-10 11:46:05 -0700

    Another ALX Task

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - printd the table
+ * @ht: the table
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+	char check = 0;
+
+	if (!ht || ht-> array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			if (check == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			check = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n")
+}
