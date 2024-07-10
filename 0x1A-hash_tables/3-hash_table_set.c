4d5dfe1ef42b00016a72f5ef95b7c9dfbabd8074
Tamir <hero2000.ts@gmail.com>
2024-07-10 11:04:07 -0700

    Another ALX Task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,35 @@
 #include "hash_tables.h"
 
 /**
+ * make_a_node - creates the node
+ * @key: the key
+ * @value: the value
+ * Return: pt to the node if success, NULL if failure
+ */
+hash_node_t *make_a_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (NULL);
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
  * hash_table_set - adds an element to the table
  * @ht: pointer to the hash table
  * @key: the key
@@ -31,22 +60,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		tmp = tmp->next;
 	}
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = make_a_node(key, value);
 	if (!new_node)
 		return (0);
-	new_node->key = strdup(key);
-	if (!new_node->key)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->value = strdup(value);
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-	new_node->next = NULL;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
