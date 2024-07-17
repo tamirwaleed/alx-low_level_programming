41423aa231550e67491e155e65b1b85ea8b82410
Tamir <hero2000.ts@gmail.com>
2024-07-17 09:12:47 -0700

    Another ALX Task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
 """ Function that returns the perimeter of the island """
 
+
 def island_perimeter(grid):
     ''' the function '''
     land = 0
-    hor = 0
-    ver = 0
-    for row in grid:
-        hor += 1
-        for cell in row:
-            ver += 1
-            if (ver == len(row)):
-                ver -= 1
-            if cell == 1:
+    rowlen = len(grid[0])
+    collen = len(grid)
+    for hor in range(collen):
+        for ver in range(rowlen):
+            if grid[hor][ver] == 1:
                 land += 4
-                if (ver == 1 or ver == len(row)):
-                    land -= 1
-                    if (ver == 1):
-                        if (row[ver] == 1):
+                if (ver == 0 or ver == rowlen - 1):
+                    if (ver == 0):
+                        if (grid[hor][ver + 1] == 1):
                             land -= 1
                     else:
-                        if (row[len(row)] == 1):
+                        if (grid[hor][ver - 1] == 1):
                             land -= 1
                 else:
-                    if (row[ver] == 1):
+                    if (grid[hor][ver + 1] == 1):
                         land -= 1
-                    if (row[ver - 2] == 1):
+                    if (grid[hor][ver - 1] == 1):
                         land -= 1
-                if (hor == 1 or hor == len(grid)):
-                    if (hor == 1):
-                        if (grid[1][ver - 1] == 1):
+                if (hor == 0 or hor == collen - 1):
+                    if (hor == 0):
+                        if (grid[1][ver] == 1):
                             land -= 1
                     else:
-                        if (grid[len(grid) - 1][ver - 1] == 1):
+                        if (grid[hor - 1][ver] == 1):
                             land -= 1
                 else:
-                    if (grid[1][ver - 1] == 1):
+                    if (grid[hor + 1][ver] == 1):
                         land -= 1
-                    if (grid[len(grid) - 1][ver - 1] == 1):
+                    if (grid[hor - 1][ver] == 1):
                         land -= 1
     return land
